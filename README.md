5fe7406d962acec8704812cea464a040881e8e13
Bhabesh Rai <brs@logpoint.com>
2020-08-04 15:09:43 +0545

    Added alerts support

--- README.md
+++ README.md
@@ -28,11 +28,45 @@ python3 third_eye.py
 
 For normal use, clone this repository and create a scheduled task to run this project for say, every 2 days.
 
+## Output
+
+### New User Detected Alert
+
+```
+Alert ID: 1
+"Generated at: 02:53 PM, Tue Aug 04, 2020"
+New user detected: Sam
+"Running query: SELECT DISTINCT processes.name, processes.path, listening_ports.port FROM listening_ports
+JOIN processes USING (pid)
+WHERE listening_ports.family = 2
+AND listening_ports.address <> '127.0.0.1';"
+At: 2020-08-02T13:22:47.907Z
+```
+
+### New Device Location Detected Alert
+
+```
+Alert ID: 10
+"Generated at: 02:59 PM, Tue Aug 04, 2020"
+New location detected: China
+For Device: Win10
+Remote IP: 104.22.88.112
+```
+
+### New Actor Detected in Audit Log Alert
+
+```
+Alert ID: 12
+New actor detected in Audit Log: Julius
+At: 2020-08-02T16:50:25.386Z
+Description: CSV Downloaded For Device Win10 - Live Query Campaign ID 2624
+```
+
 ## TODO
 
 - [x] Audit enrolled devices
 - [x] Audit live queries
 - [x] Fetch Audit logs
-- [ ] Detect queries by new authors
-- [ ] Detect abonormality in location
+- [x] Detect queries by new authors
+- [x] Detect abonormality in location
 - [ ] Visualize live query and API usage statistics
--- third_eye.py
+++ third_eye.py
@@ -5,9 +5,13 @@ from datetime import datetime
 
 # Kolide API Reference: https://kolidek2.readme.io/docs
 
-ACCESS_TOKEN = ''   # <-- Add Your Kolide access token here!!
-header = {'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)}
+# Manually set these values before running
+ACCESS_TOKEN = ''  # <-- Add Your Kolide access token here!!
+USERS = []         # <-- Add Valid Users with access to Kolide
+LOCATIONS = []     # <-- Add Valid locations
 
+header = {'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)}
+alert_count = 0
 
 def fetch_devices():
     K2_DEVICES_URL = 'https://k2.kolide.com/api/v0/devices'
@@ -22,11 +26,26 @@ def fetch_devices():
 
     with open('Devices.txt', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Generated at: {}'.format(datetime.now().strftime(
-                                                        "%I:%M %p, %a %b %d, %Y"))])
-        writer.writerow(['Total number of devices enrolled: {}\n'.format(len(data))])
-
+        writer.writerow(['Generated at: {}'.format(
+                        datetime.now().strftime("%I:%M %p, %a %b %d, %Y"))])
+        writer.writerow(['Total number of devices enrolled: {}\n'.format(
+                                                                len(data))])
+        
     for device in data:
+        if device.get('location') not in LOCATIONS:
+            with open('Alert.txt', 'a') as alertfile:
+                writer = csv.writer(alertfile)
+                global alert_count
+                alert_count += 1
+                writer.writerow(['Alert ID: {}'.format(alert_count)])
+                writer.writerow(['Generated at: {}'.format(
+                        datetime.now().strftime("%I:%M %p, %a %b %d, %Y"))])
+                writer.writerow(['New location detected: {}'.format(
+                                                   device.get('location'))])
+                writer.writerow(['For Device: {}'.format(device.get('name'))])
+                writer.writerow(['Remote IP: {}\n'.format(
+                                                    device.get('remote_ip'))])
+
         with open('Devices.txt', 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(['Device ID: {}\nDevice Name: {}\n'
@@ -56,6 +75,20 @@ def fetch_live_queries():
                          'Tables Used', 'Author Name', 'Author Email'])
 
     for query in data:
+        if query.get('author').get('name') not in USERS:
+            with open('Alert.txt', 'a') as alertfile:
+                writer = csv.writer(alertfile)
+                global alert_count
+                alert_count += 1
+                writer.writerow(['Alert ID: {}'.format(alert_count)])
+                writer.writerow(['Generated at: {}'.format(
+                        datetime.now().strftime("%I:%M %p, %a %b %d, %Y"))])
+                writer.writerow(['New user detected: {}'.format(
+                                          query.get('author').get('name'))])
+                writer.writerow(['Running query: {}'.format(
+                                                 query.get('osquery_sql'))])
+                writer.writerow(['At: {}\n'.format(query.get('created_at'))])
+
         with open('Queries.csv', 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([query.get('name'), 
@@ -79,6 +112,18 @@ def fetch_audit_logs():
         writer.writerow(['ID', 'Timestamp', 'Actor Name', 'Description'])
 
     for log in data:
+        if log.get('actor_name') not in USERS:
+            with open('Alert.txt', 'a') as alertfile:
+                writer = csv.writer(alertfile)
+                global alert_count
+                alert_count += 1
+                writer.writerow(['Alert ID: {}'.format(alert_count)])
+                writer.writerow(['New actor detected in Audit Log: {}'.format(
+                                                       log.get('actor_name'))])
+                writer.writerow(['At: {}'.format(log.get('timestamp'))])
+                writer.writerow(['Description: {}\n'.format(
+                                                      log.get('description'))])
+
         with open('Audit Logs.csv', 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([log.get('id'), log.get('timestamp'), 
